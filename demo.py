1148a0d649d5d3ac2828694ef15283b8ef978ad5
ksnithya <53986699+ksnithya@users.noreply.github.com>
2024-05-16 21:06:12 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
   
 @app.route('/') 
 def hello(): 
-    return "welcome to the flask tutorials updated with Fluxcd"
+    return "welcome to the flask tutorials updated with Fluxcd and image"
   
   
 if __name__ == "__main__": 
